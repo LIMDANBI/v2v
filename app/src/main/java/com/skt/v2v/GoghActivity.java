89b5137d7753873e991f6801fe9cf70bfe950fa9
LIMDANBI <dlaeksql08222@gmail.com>
2022-08-09 21:29:55 +0900

    layer 및 intent 구현 완료

---
+++ app/src/main/java/com/skt/v2v/GoghActivity.java
@@ -0,0 +1,14 @@
+package com.skt.v2v;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GoghActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gogh);
+    }
+}
--- app/src/main/java/com/skt/v2v/MainActivity.java
+++ app/src/main/java/com/skt/v2v/MainActivity.java
@@ -2,13 +2,37 @@ package com.skt.v2v;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton style1, style2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        style1 = (ImageButton) findViewById(R.id.style1);
+        style2 = (ImageButton) findViewById(R.id.style2);
+
+        style1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EgonActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        style2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), GoghActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
